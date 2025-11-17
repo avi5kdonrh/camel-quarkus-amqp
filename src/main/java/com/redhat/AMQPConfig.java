cdd95fa6ed135df8e64797cf29fe005dd65f5c62
Avinash Dongre <adongre@adongre-thinkpadp16vgen1.rmtin.csb>
2025-11-17 21:51:32 +0530

    test

--- src/main/java/com/redhat/AMQPConfig.java
+++ src/main/java/com/redhat/AMQPConfig.java
@@ -3,15 +3,61 @@ package com.redhat;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Named;
 import org.apache.camel.component.amqp.AMQPComponent;
+import org.apache.qpid.jms.JmsConnectionFactory;
+import org.messaginghub.pooled.jms.JmsPoolConnectionFactory;
 
 public class AMQPConfig {
 
+    /*
+     * @Produces
+     * @Named("my-amqp-caching") public AMQPComponent amqpc() throws Exception { String brokerUrl =
+     * System.getProperty("amqp.broker", "amqp://localhost:5672"); JmsConnectionFactory delegate = new
+     * JmsConnectionFactory(brokerUrl); AMQPComponent amqpComponent = new AMQPComponent(); CachingConnectionFactory
+     * connectionFactory = new CachingConnectionFactory(delegate); connectionFactory.setCacheConsumers(true);
+     * connectionFactory.setClientId("caching-client"); amqpComponent.setClientId("caching-client");
+     * amqpComponent.setConnectionFactory(connectionFactory);
+     * System.out.println(">>>>>>>>>>> Building caching component >>"); return amqpComponent; }
+     */
+
     @Produces
-    @Named("my-amqp")
-    public AMQPComponent amqp() throws Exception {
+    @Named("my-amqp-producer")
+    public AMQPComponent producer() throws Exception {
+
+        String brokerUrl = System.getProperty("amqp.broker", "amqp://localhost:5672");
+
+        JmsConnectionFactory delegate = new JmsConnectionFactory(brokerUrl);
         AMQPComponent amqpComponent = new AMQPComponent();
-        amqpComponent.setClientId("my-client");
-        System.out.println(">>>>>>>>>>> Building component >>");
+
+        JmsPoolConnectionFactory pcf = new JmsPoolConnectionFactory();
+        pcf.setConnectionFactory(delegate);
+        pcf.setMaxConnections(10);
+        pcf.setMaxSessionsPerConnection(100);
+
+        amqpComponent.setConnectionFactory(pcf);
+        System.out.println(">>>>>>>>>>> Building pooled producer >>");
         return amqpComponent;
     }
+
+    @Produces
+    @Named("my-amqp-consumer")
+    public AMQPComponent consumer() throws Exception {
+
+        String brokerUrl = System.getProperty("amqp.broker",
+                "failover:(amqp://localhost:5672)?failover.maxReconnectAttempts=20&failover.reconnectDelay=4000");
+
+        JmsConnectionFactory connectionFactory = new JmsConnectionFactory(brokerUrl);
+        connectionFactory.setClientID("myclient");
+        JmsPoolConnectionFactory pcf = new JmsPoolConnectionFactory();
+        pcf.setConnectionFactory(connectionFactory);
+        pcf.setMaxConnections(1);
+        pcf.setConnectionIdleTimeout(20000);
+        pcf.setConnectionCheckInterval(10000);
+        pcf.setMaxSessionsPerConnection(100);
+
+        AMQPComponent amqpComponent = new AMQPComponent();
+        amqpComponent.setConnectionFactory(pcf);
+        System.out.println(">>>>>>>>>>> Building pooled consumer >>");
+        return amqpComponent;
+    }
+
 }
--- src/main/java/com/redhat/AmqpRoutes.java
+++ src/main/java/com/redhat/AmqpRoutes.java
@@ -1,5 +1,7 @@
 package com.redhat;
 
+import java.util.UUID;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -13,10 +15,14 @@ public class AmqpRoutes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("master:ns:timer:bar").id("timer-consumer-route").setBody(constant(message))
-                .to("my-amqp:queue:SCIENCEQUEUE").log("Message sent from route ${routeId} to SCIENCEQUEUE");
+        from("master:ns:timer:test?period=5s").setBody(constant(UUID.randomUUID().toString())).multicast()
+                .to("my-amqp-producer:queue:SCIENCEQUEUE").to("my-amqp-producer:queue:SCIENCEQUEUE-New");
 
-        from("master:ns:my-amqp:queue:SCIENCEQUEUE?receiveTimeout=10000").id("amqp-consumer-route").id("consumer-route")
+        from("master:ns:my-amqp-consumer:queue:SCIENCEQUEUE?receiveTimeout=10000").id("amqp-consumer")
                 .to("log:MyLogger?showBody=true");
+
+        from("master:ns:my-amqp-consumer:SCIENCEQUEUE-New?receiveTimeout=10000").id("amqp-consumer1")
+                .to("log:MyLogger1?showBody=true");
+
     }
 }
